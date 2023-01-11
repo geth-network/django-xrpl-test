ce4178e0fd58113b7f60d4781e191de02275b21e
Stanislav Kachnov <rileydeq@gmail.com>
2023-01-11 08:51:23 +0100

    add CSRF_TRUSTED_ORIGINS to settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,7 +9,7 @@ DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
-
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 if 'whitenoise.middleware.WhiteNoiseMiddleware' in MIDDLEWARE:
     MIDDLEWARE.remove("whitenoise.middleware.WhiteNoiseMiddleware")
 # DATABASES
